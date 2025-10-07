ca69815e7856583d32acf3ca06fd206160558b08
trey-rosius <treyrosius@gmail.com>
2025-10-07 11:39:54 +0100

    first commit

--- lib/ai_writer-stack.ts
+++ lib/ai_writer-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class AiWriterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create the DynamoDB table with a single-table design
+    const ai_writer_db = new dynamodb.Table(this, "AiWriterTable", {
+      tableName: "ai-writer-table",
+      partitionKey: {
+        name: "PK",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "SK",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AiWriterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      pointInTimeRecoverySpecification: {
+        pointInTimeRecoveryEnabled: false,
+      },
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
+    });
+
+    // Create an S3 bucket for storing generated videos and thumbnails
+    const mediaBucket = new s3.Bucket(this, "S3MediaBucket", {
+      bucketName: `${cdk.Stack.of(this).account}-${
+        cdk.Stack.of(this).region
+      }-media-bucket`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+      lifecycleRules: [
+        {
+          id: "DeleteOldVersions",
+          enabled: true,
+          noncurrentVersionExpiration: cdk.Duration.days(30),
+        },
+      ],
+    });
   }
 }
